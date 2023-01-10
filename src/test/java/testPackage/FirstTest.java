02f3bcc31e02b579d9be51af6c901f9e6c5cc6bd
vmakar9 <zmerin212@gmail.com>
2023-01-10 18:04:55 +0200

    Initial commit

---
+++ src/test/java/testPackage/FirstTest.java
@@ -0,0 +1,8 @@
+import org.testng.annotations.Test;
+
+public class FirstTest {
+    @Test
+    public void test(){
+        System.out.println("This is my first test");
+    }
+}
---
+++ src/test/java/testPackage/SecondTest.java
@@ -0,0 +1,8 @@
+import org.testng.annotations.Test;
+
+public class SecondTest {
+    @Test
+    public void test2(){
+        System.out.println("Second test");
+    }
+}
