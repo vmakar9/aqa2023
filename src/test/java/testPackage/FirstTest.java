254d85e2cbf51db04273ca21c2c7455c6f61d699
vmakar9 <zmerin212@gmail.com>
2023-01-17 19:24:24 +0200

    1

--- src/test/java/testPackage/FirstTest.java
+++ src/test/java/testPackage/FirstTest.java
@@ -3,8 +3,10 @@ package testPackage;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
 import org.base.BaseTest;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.testng.annotations.*;
 
 import static com.codeborne.selenide.Selenide.$;
@@ -16,11 +18,42 @@ public class FirstTest extends BaseTest {
 
     @Test
     public void test(){
-       // $(By.className("submit-button")).click();
-       // $(By.id());
-        $(By.xpath("//input[@placeholder = 'Username']")).append("standard_user");
-      ElementsCollection inventoryList = $$(By.xpath("//div[@class='inventory_list']//child::div[@class='inventory_item']"));
-        Selenide.sleep(5000);
+      $(By.xpath("//input[@placeholder='Username']")).append("standard_user");
+      $(By.xpath("//input[@placeholder='Password']")).append("secret_sauce");
+      $(By.xpath("//input[@type='submit']")).click();
+//      $(By.xpath("xpath")).doubleClick();
+
+//        $(By.xpath("//input[@placeholder='Username']")).clear();
+//        Selenide.sleep(3000);
+//        $(By.xpath("//input[@placeholder='Password']")).clear();
+//        Selenide.sleep(3000);
+
+//       $(By.xpath("//input[@id = 'twotabsearchtextbox' ]")).append("Java");
+//       Selenide.actions().sendKeys(Keys.ENTER).perform();
+       Selenide.sleep(1000);
+//        $(By.xpath("//select[@class = 'product_sort_container']")).click();
+//        Selenide.actions().sendKeys(Keys.ESCAPE).perform();
+//        Selenide.actions().keyDown($(By.xpath("xpath_from_element")),Keys.SHIFT).perform();
+//        Selenide.actions().keyUp($(By.xpath("To_element_target")),Keys.SHIFT).perform();
+//        Selenide.actions().dragAndDrop($(By.xpath("source_element")),$(By.xpath("target_element")));
+//        Selenide.actions().dragAndDropBy($(By.xpath("element")),200,0);
+
+//        $(By.xpath("//nav[@class='nav-menu']//div[contains(@class, 'nav-menu-has-children')]")).hover();
+
+//        System.out.println($(By.xpath("(//div[@class='inventory_item_name'])[1]")).text());
+
+//        for(String text : $$(By.xpath("//div[@class='inventory_item_name']")).texts()){
+//            System.out.println(text);
+//        }
+
+          ElementsCollection elements = $$(By.xpath("//div[@class='inventory_item_name']"));
+        for (SelenideElement element : elements) {
+            System.out.println(element);
+        }
+//
+//        Selenide.sleep(1000);
+
+
     }
 
 
